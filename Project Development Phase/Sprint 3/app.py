9239a06d77ca6996fde7ca929ce46f8d4ba6ffc4
Anandh-23 <92389085+Anandh-23@users.noreply.github.com>
2022-11-20 00:38:14 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -15,46 +15,45 @@ app.config['UPLOAD_FOLDER'] = picFolder
 
 @app.route('/dashboard', methods = ['GET','POST'])
 def dashboard():
-	age = int(request.form['age'])
-	bp  = float(request.form['bp'])
-	sp  = float(request.form['sp'])
-	al  = float(request.form['al'])
-	su  = float(request.form['su'])
-	rbc = float(request.form['rbc'])
-	pc  = float(request.form['pc'])
-	pcc = float(request.form['pcc'])
-	bgr = float(request.form['bgr'])
-	bu = float(request.form['bu'])
-	sc = float(request.form['sc'])
-	sod = float(request.form['sod'])
-	pot = float(request.form['pot'])
-	hemo = float(request.form['hemo'])
-	pcv = float(request.form['pcv'])
-	wc = float(request.form['wc'])
-	htn = float(request.form['htn'])
-	dm = float(request.form['dm'])
-	cad = float(request.form['cad'])
-	appet = float(request.form['appet'])
-	pe = float(request.form['pe'])
-	ane = float(request.form['ane'])
-	ba = float(request.form['ba'])
-	cls = float(request.form['class'])
-	rc = float(request.form['rc'])
-	prediction=model.predict([[age, bp, sp, al, su, rbc, pc, pcc, bgr, bu, sc, sod, pot, hemo, pcv, wc, htn, dm, cad, appet, pe, ane, ba,cls,rc]])
-	output = np.round(prediction)
-	if output<0:
-		return render_template('dash.html',prediction_texts="Sorry! You have Chronic Kidney Disese...")
-	return render_template('dash.html',prediction_text="Congrats!! You don't have Chronic Kidney Disease...")
-
+	if request.method=='POST':
+		age = int(request.form['age'])
+		bp  = float(request.form['bp'])
+		sp  = float(request.form['sp'])
+		al  = float(request.form['al'])
+		su  = float(request.form['su'])
+		rbc = float(request.form['rbc'])
+		pc  = float(request.form['pc'])
+		pcc = float(request.form['pcc'])
+		bgr = float(request.form['bgr'])
+		bu = float(request.form['bu'])
+		sc = float(request.form['sc'])
+		sod = float(request.form['sod'])
+		pot = float(request.form['pot'])
+		hemo = float(request.form['hemo'])
+		pcv = float(request.form['pcv'])
+		wc = float(request.form['wc'])
+		htn = float(request.form['htn'])
+		dm = float(request.form['dm'])
+		cad = float(request.form['cad'])
+		appet = float(request.form['appet'])
+		pe = float(request.form['pe'])
+		ane = float(request.form['ane'])
+		ba = float(request.form['ba'])
+		prediction=model.predict([[age, bp, sp, al, su, rbc, pc, pcc, bgr, bu, sc, sod, pot, hemo, pcv, wc, htn, dm, cad, appet, pe, ane, ba]])
+		output = np.round(prediction)
+		if output==0:
+			return render_template('dash.html',prediction_text="Sorry! You have Chronic Kidney Disese...")
+		return render_template('dash.html',prediction_text="Congrats!! You don't have Chronic Kidney Disease...")
+	return render_template('dashboard.html')
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
 def login():
 	msg = ' '
 	loginPic = os.path.join(app.config['UPLOAD_FOLDER'],'login.png')
-	if request.method == 'POST' and 'usrname' in request.form and 'password' in request.form:
+	if request.method == 'POST' and 'user' in request.form and 'password' in request.form:
 		username = request.form['user']
 		password = request.form['password']
-		sql = "SELECT * FROM signup WHERE username = '"+username+"' AND pass = '"+password+"'"
+		sql = "SELECT * FROM signup WHERE username = '"+username+"' AND password = '"+password+"'"
 		stmt = ibm_db.exec_immediate(conn, sql)
 		account = ibm_db.fetch_both(stmt)
 		if account:
@@ -62,7 +61,7 @@ def login():
 			session['id'] = account[0]
 			session['username'] = account[1]
 			msg = 'Logged in successfully !'
-			return redirect(url_for('dashboard'))
+			return redirect(url_for('dashboard',text = msg))
 		else:
 			msg = 'Incorrect username / password !'
 	return render_template('log_in.html', errorMsg = msg,loginpic = loginPic)
@@ -71,9 +70,9 @@ def login():
 def register():
 	msg = ''
 	registerImg = os.path.join(app.config['UPLOAD_FOLDER'],'login.png')
-	if request.method == 'POST' and 'usrname' in request.form and 'password' in request.form and 'email' in request.form :
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
 		username = request.form['username']
-		password = request.form['pswd']
+		password = request.form['password']
 		email = request.form['mail']
 		sql = "SELECT * FROM signup WHERE username = '"+username+"'"
 		stmt = ibm_db.exec_immediate(conn, sql)
@@ -92,6 +91,8 @@ def register():
 			prep_stmt = ibm_db.prepare(conn, ins)
 			ibm_db.execute(prep_stmt)
 			msg = 'You have successfully registered !'
+			return redirect(url_for('login'))
+
 	elif request.method == 'POST':
 		msg = 'Please fill out form !'
 	return render_template('signup.html', errorMsg = msg,registerImg=registerImg)
@@ -102,4 +103,3 @@ if __name__=="__main__":
 
 
     
-	
